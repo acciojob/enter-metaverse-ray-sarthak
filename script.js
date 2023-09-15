006a186745289e452fc6b37e52b66afbddce8161
ray-sarthak <117006899+ray-sarthak@users.noreply.github.com>
2023-09-16 01:48:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const para=document.getElementById("status");
+const header=document.createElement("h1");
+const body=document.getElementsByTagName("body")[0];
+header.textContent="Entered Metaverse";
+function myfunction(){
+	body.removeChild(para);
+	body.appendChild(header);
+}
+
